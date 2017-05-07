0f21b5f48eeba67806a9e7ee36328084c5f56749
Guillaume de Moffarts <guillaumedemoff@gmail.com>
2017-05-07 17:59:34 +0200

    add feelTheMagic function and adapt all place

--- actions.py
+++ actions.py
@@ -1,5 +1,5 @@
 import json
-state = [[[0,1,0,1],[0,1,0,1],[0,1,0,1],[0,1,0,1]],[[0,1,0],[1,0,1],[0,1,0]],[[1,None],[None,None]],[[None]]]
+
 state = [[[0,1,0,1],[0,1,0,1],[0,1,None,None],[None,None,None,None]],[[None,None,None],[None,None,None],[None,None,None]],[[None,None],[None,None]],[[None]]]
 def allPlace(state):
     moves = []
@@ -7,17 +7,21 @@ def allPlace(state):
         for row in range(4-layer):
             for column in range(4-layer):
                 if state[layer][row][column] == None:
-                    feelTheMagic = False
-                    for i in range(2):
-                        for j in range(2):
-                            if layer != 0:
-                                if state[layer-1][row+i][column+j] == None and layer != 0:
-                                    feelTheMagic = True
-                    move = json.dumps({
-                        'move': 'place',
-                        'to': [layer, row, column]
-                    })
-                    if not feelTheMagic:
+
+                    if not feelTheMagic(state, layer, row, column):
+                        move = json.dumps({
+                            'move': 'place',
+                            'to': [layer, row, column],
+                            'cost': -1
+                        })
                         moves.append(move)
-    print(moves)
-allPlace(state)
+    return moves
+
+def feelTheMagic(state, layer, row, column):
+    feelTheMagic = False
+    for i in range(2):
+        for j in range(2):
+            if layer != 0:
+                if state[layer-1][row+i][column+j] == None and layer != 0:
+                    feelTheMagic = True
+    return feelTheMagic
